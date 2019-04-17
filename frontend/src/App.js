52f82d3751dca3062319770491fde3cc5437a407
Niki Ahlskog <niki.ahlskog@gmail.com>
2019-04-17 15:09:27 +0300

    PTOSKKO-34

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Button } from 'semantic-ui-react';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css';
+import axios from 'axios';
+
+async function getApiData() {
+  try {
+    const response = await axios.get('http://localhost:1337/testendpoints');
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Button primary onClick={getApiData}>Very semantic button</Button>
         </header>
       </div>
     );

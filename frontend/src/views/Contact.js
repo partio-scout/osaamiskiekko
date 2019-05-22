a3451e3901d523bcf3b1e679ba1f5967f6b6c323
Niki Ahlskog <niki.ahlskog@gmail.com>
2019-05-22 16:15:28 +0300

    other pages

--- frontend/src/views/Contact.js
+++ frontend/src/views/Contact.js
@@ -5,30 +5,14 @@ import headerImage from '../images/headerimage.jpg';
 
 const S = {};
 S.Home = styled.div`
-  ::before {
-    content: " ";
-    position: absolute;
-    width: 100%;
-    height: 1100px;
-    z-index: -1;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-  }
-  ::after {
-     content: "";
-    position: absolute;
-    top: 0;
-    right: 0;
-    width: 100%;
-    background-repeat: no-repeat;
-    height: 100%;
-    background-size: cover;
-    z-index: -10;
-    background-position: 100% 80%;
-    background-image: url(${headerImage});
-  }
+  max-width: 1440px;
+  margin: auto;
+  padding-left: 20px;
+  padding-right: 20px;
 
+  @media only screen and (max-width: 860px) {
+    padding: 0px;
+  } 
 `;
 
 const Contact = () => {

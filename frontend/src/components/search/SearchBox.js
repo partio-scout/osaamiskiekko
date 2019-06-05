bf3fe7853bcd2e594a1f52b842363b0e16599706
Esko Rikkonen <esko.rikkonen@eficode.com>
2019-06-05 11:42:53 +0300

    Refactors SearchBox and results and moves them to Home view

--- frontend/src/components/search/SearchBox.js
+++ frontend/src/components/search/SearchBox.js
@@ -5,20 +5,17 @@ import { css } from '@emotion/core';
 import { BarLoader } from 'react-spinners';
 import { useGlobalStateContext } from '../../utils/GlobalStateContext';
 import OutsideClickHandler from 'react-outside-click-handler';
-import { FormattedMessage } from 'react-intl';
 
 const S = {};
 S.SearchBox = styled.div`
   background-color: #FFFFFF;
  	box-shadow: 0 2px 8px 0 rgba(0,0,0,0.24);
   border-radius: 8px;
-  margin-top: -50px;
+  margin-top: -500px;
   z-index: 20;
-  position: absolute;
+  position: relative;
   margin-left: auto;
   margin-right: auto;
-  left: 0;
-  right: 0;
   width: 587px;
   display: flex;
   justify-content: center;
@@ -37,14 +34,6 @@ S.SearchBox = styled.div`
     }
   }
 
-  .results-amount {
-    position: absolute;
-    bottom: -75px;
-  }
-  .examination-number {
-    padding: 5px;
-  }
-
   @media only screen and (max-width: 767px) {
     width: 85%;
     padding: 16px;
@@ -57,7 +46,7 @@ const loadingSpinnerOverride = css`
 `;
 
 export default function SearchBox(props) {
-  const { showResults, data, isLoading, examinationAmount } = props;
+  const { showResults, data, isLoading } = props;
   const globalState = useGlobalStateContext();
 
   const [institutionInput, setInstitutionInput] = useState('');
@@ -194,14 +183,6 @@ export default function SearchBox(props) {
         </OutsideClickHandler>
           }
         </div>
-      }
-      {
-        examinationAmount > 0 &&
-        <p className="results-amount">
-          <FormattedMessage id="examination.info" />
-            <span className="examination-number">{examinationAmount}</span>
-          <FormattedMessage id="examination.infoEnd" />
-        :</p>
       }
     </S.SearchBox>
   )

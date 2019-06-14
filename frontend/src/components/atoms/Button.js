746804987adda421fd4ee0fdd65c1f2d7d0e25b9
Niki Ahlskog <niki.ahlskog@gmail.com>
2019-06-14 15:28:09 +0300

    chrome and safari social media share test

--- frontend/src/components/atoms/Button.js
+++ frontend/src/components/atoms/Button.js
@@ -4,41 +4,48 @@ import classnames from 'classnames'
 
 const S = {};
 S.Button = styled.button`
-margin: auto;
 font-size: 16px;	
 line-height: 24px;	
 text-align: center;
 border-radius: 8px;	
 border: none;
-color: ${props => props.theme.colors.textColorLight}
-background-color: ${props => props.theme.colors.highlight}
+color: ${props => props.theme.colors.textColorLight};
+background-color: ${props => props.theme.colors.highlight};
 padding: 12px 10px; 
 text-transform: uppercase;
 letter-spacing: 4px;
 cursor: pointer;
 
+.button-icon:before {
+  font-size: 20px;
+  color: white;
+  margin-left: 5px;
+  float: right;
+}
+
+
 :hover, :focus {
-  -webkit-box-shadow: 7px 7px 0 0 ${props => props.theme.colors.textHighlight}
-  -moz-box-shadow: 7px 7px 0 0 ${props => props.theme.colors.textHighlight}
-  box-shadow: 7px 7px 0 0 ${props => props.theme.colors.textHighlight}
+  -webkit-box-shadow: 7px 7px 0 0 ${props => props.theme.colors.textHighlight};
+  -moz-box-shadow: 7px 7px 0 0 ${props => props.theme.colors.textHighlight};
+  box-shadow: 7px 7px 0 0 ${props => props.theme.colors.textHighlight};
 }
 
 &.active {
-  border: 1px solid ${props => props.theme.colors.highlight}
-  background-color: ${props => props.theme.colors.textColorLight}
-  color: ${props => props.theme.colors.highlight}
+  border: 1px solid ${props => props.theme.colors.highlight};
+  background-color: ${props => props.theme.colors.textColorLight};
+  color: ${props => props.theme.colors.highlight};
 
   :hover, :focus {
-    -webkit-box-shadow: 7px 7px 0 0 ${props => props.theme.colors.highlight}
-    -moz-box-shadow: 7px 7px 0 0 ${props => props.theme.colors.highlight}
-    box-shadow: 7px 7px 0 0 ${props => props.theme.colors.highlight}
+    -webkit-box-shadow: 7px 7px 0 0 ${props => props.theme.colors.highlight};
+    -moz-box-shadow: 7px 7px 0 0 ${props => props.theme.colors.highlight};
+    box-shadow: 7px 7px 0 0 ${props => props.theme.colors.highlight};
   }
 }
 `;
 
 export default function Curve(props) {
-  const { onClick, value, className, active } = props;
-
+  const { onClick, value, className, active, icon } = props;
+  const shareButton = icon ? {float : 'right'} : undefined;
   const classes = classnames(
     className, 
     {
@@ -47,8 +54,9 @@ export default function Curve(props) {
   )
 
   return (
-    <S.Button onClick={onClick} className={classes}>
+    <S.Button onClick={onClick} className={classes} style={shareButton}>
       {value}
+      {icon ? <i className={`button-icon ${icon}`} style={{ marginRight: '0px' }}></i> : undefined}
     </S.Button>
   );
 }

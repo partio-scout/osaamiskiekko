b32b7876f70cf048c1c9c2b48b7b7219664f48b6
Niki Ahlskog <niki.ahlskog@gmail.com>
2019-06-19 11:21:38 +0300

    added google tags manager

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -69,6 +69,23 @@ const App = () => {
       <Helmet>
         <html lang={language} />
         <title>{translations[language].title}</title>
+        <script>
+          {`
+          (function(w,d,s,l,i){w[l] = w[l] || []; w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-NK65GGN');
+          `}
+          </script>
+        
+          <noscript>
+          {`
+            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NK65GGN"
+              height="0" width="0" style="display:none;visibility:hidden">
+            </iframe>
+          `}
+          </noscript>
       </Helmet>
       <GlobalStateContext.Provider value={globalState}>
         <ThemeProvider theme={currentTheme}>

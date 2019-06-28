4436dbbb4279cd1105fbfb964d1d94701d5d40ef
mrinta-homi <mikko.rinta-homi@eficode.com>
2019-06-28 09:36:30 +0300

    Tring to get resultCarousel test work

--- frontend/src/__tests__/resultsCarousel.test.js
+++ frontend/src/__tests__/resultsCarousel.test.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { configure, shallow } from 'enzyme';
+import { configure, shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import ResultsCarousel from '../components/search/ResultsCarousel';
 import { fieldOfStudiesData } from '../__testdata__/';
+import { schoolData } from '../__testdata__/';
+import { IntlProvider } from 'react-intl';
+import * as testHelper from '../testHelpers';
 
 configure({ adapter: new Adapter() });
 
 describe('Results carousel renders items', () => {
   test('First label is correct', () => {
-    const carousel = shallow(
-      <ResultsCarousel sortedCarouselFields={fieldOfStudiesData} />
-    ).dive()
-    expect(carousel.find('.carousel-item div p').first().text()).toEqual('Humanistiset alat ja teologia')
-    expect(carousel.find('.credits-length').first().text()).toEqual('(0)')
+    const carousel = shallow(testHelper.wrapWithProviders(<ResultsCarousel sortedCarouselFields={fieldOfStudiesData} setSelectedCarouselField={schoolData[0]} />));
+    expect(carousel.find('.carousel-item div p').first().text()).toEqual('Humanistiset alat ja teologia');
+    expect(carousel.find('.credits-length').first().text()).toEqual('(0)');
   })
 });
